6e9bf3c3da420c5d34951b8e5dc238095525802c
saipratyushreddy <saipratyushreddy@gmail.com>
2021-06-26 14:47:53 +0530

    navbar change

--- src/components/NavBar/homeNavbar.js
+++ src/components/NavBar/homeNavbar.js
@@ -8,8 +8,8 @@ export default class Navbar extends React.Component{
             <div>
                 <nav className="navbar bg-success shadow rounded">
                     <div className="container text-white">
-                        <div className="h1 text-start fw-bolder" id="logo">GROCERY STORE</div>
-                        <ul class="nav justify-content-end">
+                        <div className="h1 text-start fw-bolder col-md-6 mx-auto my-auto" id="logo">GROCERY STORE</div>
+                        <ul class="nav justify-content-end col-md-6 mx-auto my-auto">
                             <a className="btn btn-success" href="/login">Login</a>
                         </ul>
                     </div>
--- src/components/NavBar/vendorNavbar.js
+++ src/components/NavBar/vendorNavbar.js
@@ -16,8 +16,8 @@ class Navbar extends React.Component{
             <div>
                 <nav className="navbar bg-success shadow rounded">
                     <div className="container text-white">
-                        <div className="h1 text-start fw-bolder" id="logo">GROCERY STORE</div>
-                        <ul class="nav justify-content-end">
+                        <div className="h1 text-start fw-bolder col-lg-6 mx-auto my-auto" id="logo">GROCERY STORE</div>
+                        <ul class="nav justify-content-end col-lg-6 mx-auto my-auto">
                             <li class="nav-item">
                                 <a className="btn btn-success" href="/vendor-home" >Home</a>
                             </li>
